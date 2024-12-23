4be3d768055fb06a8ad0c0a351dc33ec651b23ff
Ruslan Priadko <r.priadko@ukr.net>
2024-12-23 11:13:28 +0200

    addressed brand type annotation

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ class Car:
     def __init__(self,
                  comfort_class: int,
                  clean_mark: int,
-                 brand: int) -> None:
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
